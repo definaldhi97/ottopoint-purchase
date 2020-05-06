582ae4e886a88f098df514609758c248928745f9
Abdul Rohmat <abdul.rohmat@indoalliz.com>
2020-05-07 04:37:49 +0700

    rename service

--- services/useVoucher_uv.go
+++ services/useVoucher_uv.go
@@ -1,52 +1,164 @@
 package services
 
 import (
+	"encoding/json"
 	"errors"
-	opl "ottopoint-purchase/hosts/opl/host"
+	"ottopoint-purchase/constants"
+	"ottopoint-purchase/db"
+	uv "ottopoint-purchase/hosts/ultra_voucher/host"
+	uvmodels "ottopoint-purchase/hosts/ultra_voucher/models"
 	"ottopoint-purchase/models"
+	"ottopoint-purchase/models/dbmodels"
 	"ottopoint-purchase/utils"
 
+	"github.com/astaxie/beego/logs"
 	"github.com/opentracing/opentracing-go"
 	"go.uber.org/zap"
 )
 
-type UseVoucherUVServices struct {
-	General models.GeneralModel
-}
-
-func (t UseVoucherUVServices) UseVoucherUV(req models.UseVoucherUVReq, param models.Params, campaignID string) models.Response {
+func (t UseVoucherServices) GetVoucherUV(req models.UseVoucherReq, param models.Params) models.Response {
 	var res models.Response
-
-	var useUV interface{}
-	var reqUV interface{}
 
 	sugarLogger := t.General.OttoZaplog
 	sugarLogger.Info("[GetVoucherUV-Services]",
 		zap.String("AccountNumber : ", param.AccountNumber), zap.String("InstitutionID : ", param.InstitutionID),
-		zap.String("category : ", param.Category), zap.String("campaignId : ", campaignID),
-		zap.String("VoucherCode : ", req.VoucherCode))
+		zap.String("category : ", param.Category), zap.String("campaignId : ", req.CampaignID),
+		zap.String("cust_id : ", req.CustID), zap.String("cust_id2 : ", req.CustID2),
+		zap.String("product_code : ", param.ProductCode))
 
 	span, _ := opentracing.StartSpanFromContext(t.General.Context, "[GetVoucherUV]")
 	defer span.Finish()
 
-	// Use Voucher to Openloyalty
-	_, err2 := opl.CouponVoucherCustomer(campaignID, param.CouponID, param.ProductCode, param.CustID, 1)
-	if err2 != nil {
+	// // Use Voucher to Openloyalty
+	// _, err2 := opl.CouponVoucherCustomer(req.CampaignID, param.CouponID, param.ProductCode, param.CustID, 1)
+	// if err2 != nil {
+	// 	res = utils.GetMessageResponse(res, 400, false, errors.New("Gagal Use Voucher, Harap coba lagi"))
+	// 	return res
+	// }
 
-		go SaveTransactionUV(param, useUV, reqUV, req, "Payment", "01", "")
+	get, errGet := db.GetVoucherUV(param.AccountNumber, param.CouponID)
+	if errGet != nil || get.AccountId == "" {
+		logs.Info("Internal Server Error : ", errGet)
+		logs.Info("[GetVoucherUV-Servcies]-[GetVoucherUV]")
+		logs.Info("[Failed get data from DB]")
 
-		res = utils.GetMessageResponse(res, 400, false, errors.New("Gagal Use Voucher, Harap coba lagi"))
+		// sugarLogger.Info("Internal Server Error : ", errGet)
+		sugarLogger.Info("[GetVoucherUV-Servcies]-[GetVoucherUV]")
+		sugarLogger.Info("[Failed get data from DB]")
+
+		res = utils.GetMessageResponse(res, 422, false, errors.New("Voucher Tidak Ditemukan"))
 		return res
 	}
 
-	go SaveTransactionUV(param, useUV, reqUV, req, "Payment", "00", "00")
+	comulative_ref := utils.GenTransactionId()
+	param.Reffnum = comulative_ref
+	param.Amount = int64(param.Point)
 
-	res = models.Response{
-		Meta: utils.ResponseMetaOK(),
-		Data: models.UseVoucherUVResp{
-			Voucher: param.NamaVoucher,
-		},
+	reqUV := uvmodels.UseVoucherUVReq{
+		Account:     get.AccountId,
+		VoucherCode: get.VoucherCode,
 	}
-	return res
 
+	// get to UV
+	useUV, errUV := uv.UseVoucherUV(reqUV)
+	if errUV != nil || useUV.ResponseCode == "" {
+		logs.Info("Internal Server Error : ", errUV)
+		logs.Info("[GetVoucherUV-Servcies]-[UseVoucherUV]")
+		logs.Info("[Failed Use Voucher UV]-[Gagal Use Voucher UV]")
+
+		// sugarLogger.Info("Internal Server Error : ", errUV)
+		sugarLogger.Info("[GetVoucherUV-Servcies]-[UseVoucherUV]")
+		sugarLogger.Info("[Failed Use Voucher UV]-[Gagal Use Voucher UV]")
+
+		res = utils.GetMessageResponse(res, 129, false, errors.New("Voucher Gagal Digunakan, Silahkan Coba Beberapa Saat Lagi"))
+		// res.Data = "Transaksi Gagal"
+		return res
+	}
+
+	if useUV.ResponseCode == "14" {
+
+		go SaveTransactionUV(param, useUV, reqUV, req, "Inquiry", "01", useUV.ResponseCode)
+
+		res = utils.GetMessageResponse(res, 148, false, errors.New("Voucher Sudah Digunakan"))
+		// res.Data = "Transaksi Gagal"
+
+		return res
+	}
+
+	if useUV.ResponseCode == "10" {
+
+		go SaveTransactionUV(param, useUV, reqUV, req, "Inquiry", "01", useUV.ResponseCode)
+
+		res = utils.GetMessageResponse(res, 147, false, errors.New("Voucher Tidak Ditemukan"))
+		// res.Data = "Transaksi Gagal"
+
+		return res
+	}
+
+	if useUV.ResponseCode == "00" {
+		go SaveTransactionUV(param, useUV, reqUV, req, "Inquiry", "00", useUV.ResponseCode)
+
+		res = models.Response{
+			Meta: utils.ResponseMetaOK(),
+			Data: models.GetVoucherUVResp{
+				Voucher:     param.NamaVoucher,
+				VoucherCode: get.VoucherCode,
+				Link:        useUV.Data.Link,
+			},
+		}
+		return res
+	}
+
+	return res
+}
+
+func SaveTransactionUV(param models.Params, res interface{}, reqdata interface{}, reqOP interface{}, trasnType, status, rc string) {
+
+	logs.Info("[Start-SaveDB]-[UltraVoucher]")
+
+	var saveStatus string
+	switch status {
+	case "00":
+		saveStatus = constants.Success
+	case "09":
+		saveStatus = constants.Pending
+	case "01":
+		saveStatus = constants.Failed
+	}
+
+	reqUV, _ := json.Marshal(&reqdata)   // Req Ottoag
+	responseUV, _ := json.Marshal(&res)  // Response Ottoag
+	reqdataOP, _ := json.Marshal(&reqOP) // Req Service
+
+	save := dbmodels.TransaksiRedeem{
+		AccountNumber:   param.AccountNumber,
+		Voucher:         param.NamaVoucher,
+		MerchantID:      param.MerchantID,
+		CustID:          param.CustID,
+		RRN:             param.RRN,
+		ProductCode:     param.ProductCode,
+		Amount:          int64(param.Amount),
+		TransType:       trasnType,
+		ProductType:     "UltraVoucher",
+		Status:          saveStatus,
+		ExpDate:         param.ExpDate,
+		Institution:     param.InstitutionID,
+		CummulativeRef:  param.Reffnum,
+		DateTime:        utils.GetTimeFormatYYMMDDHHMMSS(),
+		ResponderData:   status,
+		Point:           param.Point,
+		ResponderRc:     rc,
+		RequestorData:   string(reqUV),
+		ResponderData2:  string(responseUV),
+		RequestorOPData: string(reqdataOP),
+		SupplierID:      param.SupplierID,
+	}
+
+	err := db.DbCon.Create(&save).Error
+	if err != nil {
+		logs.Info("[Failed Save to DB ]", err)
+		logs.Info("[Package-Voucher]-[Service-RedeemPulsa]")
+		// return err
+
+	}
 }
