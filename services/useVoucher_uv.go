2cddee27269617cf459f35dc6f7550ce7beaf374
Abdul Rohmat <abdul.rohmat@indoalliz.com>
2020-05-11 16:49:06 +0700

    response uv

--- services/useVoucher_uv.go
+++ services/useVoucher_uv.go
@@ -3,6 +3,7 @@ package services
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"ottopoint-purchase/constants"
 	"ottopoint-purchase/db"
 	uv "ottopoint-purchase/hosts/ultra_voucher/host"
@@ -61,32 +62,10 @@ func (t UseVoucherServices) GetVoucherUV(req models.UseVoucherReq, param models.
 
 	// get to UV
 	useUV, errUV := uv.UseVoucherUV(reqUV)
-	if errUV != nil || useUV.ResponseCode == "" {
-		logs.Info("Internal Server Error : ", errUV)
-		logs.Info("[GetVoucherUV-Servcies]-[UseVoucherUV]")
-		logs.Info("[Failed Use Voucher UV]-[Gagal Use Voucher UV]")
-
-		// sugarLogger.Info("Internal Server Error : ", errUV)
-		sugarLogger.Info("[GetVoucherUV-Servcies]-[UseVoucherUV]")
-		sugarLogger.Info("[Failed Use Voucher UV]-[Gagal Use Voucher UV]")
-
-		res = utils.GetMessageResponse(res, 129, false, errors.New("Voucher Gagal Digunakan, Silahkan Coba Beberapa Saat Lagi"))
-		// res.Data = "Transaksi Gagal"
-		return res
-	}
-
-	if useUV.ResponseCode == "14" {
-
-		go SaveTransactionUV(param, useUV, reqUV, req, "Inquiry", "01", useUV.ResponseCode)
-
-		res = utils.GetMessageResponse(res, 148, false, errors.New("Voucher Sudah Digunakan"))
-		// res.Data = "Transaksi Gagal"
-
-		return res
-	}
 
 	if useUV.ResponseCode == "10" {
 
+		fmt.Sprint("[Response UV : %v]", useUV.ResponseCode)
 		go SaveTransactionUV(param, useUV, reqUV, req, "Inquiry", "01", useUV.ResponseCode)
 
 		res = utils.GetMessageResponse(res, 147, false, errors.New("Voucher Tidak Ditemukan"))
@@ -95,7 +74,21 @@ func (t UseVoucherServices) GetVoucherUV(req models.UseVoucherReq, param models.
 		return res
 	}
 
+	if useUV.ResponseCode == "14" {
+		fmt.Sprint("[Response UV : %v]", useUV.ResponseCode)
+		res = models.Response{
+			Meta: utils.ResponseMetaOK(),
+			Data: models.GetVoucherUVResp{
+				Voucher:     param.NamaVoucher,
+				VoucherCode: get.VoucherCode,
+				Link:        useUV.Data.Link,
+			},
+		}
+		return res
+	}
+
 	if useUV.ResponseCode == "00" {
+		fmt.Sprint("[Response UV : %v]", useUV.ResponseCode)
 		go SaveTransactionUV(param, useUV, reqUV, req, "Inquiry", "00", useUV.ResponseCode)
 
 		res = models.Response{
@@ -106,6 +99,21 @@ func (t UseVoucherServices) GetVoucherUV(req models.UseVoucherReq, param models.
 				Link:        useUV.Data.Link,
 			},
 		}
+		return res
+	}
+
+	if errUV != nil || useUV.ResponseCode == "" || useUV.ResponseCode != "00" {
+		fmt.Sprint("[Response UV : %v]", useUV.ResponseCode)
+		logs.Info("Internal Server Error : ", errUV)
+		logs.Info("[GetVoucherUV-Servcies]-[UseVoucherUV]")
+		logs.Info("[Failed Use Voucher UV]-[Gagal Use Voucher UV]")
+
+		// sugarLogger.Info("Internal Server Error : ", errUV)
+		sugarLogger.Info("[GetVoucherUV-Servcies]-[UseVoucherUV]")
+		sugarLogger.Info("[Failed Use Voucher UV]-[Gagal Use Voucher UV]")
+
+		res = utils.GetMessageResponse(res, 129, false, errors.New("Voucher Gagal Digunakan, Silahkan Coba Beberapa Saat Lagi"))
+		// res.Data = "Transaksi Gagal"
 		return res
 	}
 
