bcb405b39a35ebe1f5962b0d1fef1996ac45cab2
Taufik Triantono <taufik.triantono@ottodigital.id>
2020-11-13 14:16:14 +0700

    fix - update voucher is_used true

--- models/voucherAg.go
+++ models/voucherAg.go
@@ -2,6 +2,6 @@ package models
 
 type GetVoucherAgResp struct {
 	Voucher     string `json:"voucher"`
-	VoucherCode string `json:"voucher_code"`
+	VoucherCode string `json:"voucherCode"`
 	Link        string `json:"link"`
 }
