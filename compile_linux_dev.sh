00c1e537088595df715d15affbb4c7a75d30e312
Abdul Rohmat <abdul.rohmat@indoalliz.com>
2021-02-23 00:27:46 +0700

    switch new vendor

--- compile_linux_dev.sh
+++ compile_linux_dev.sh
@@ -1,6 +1,6 @@
 
 env GOOS=linux GOARCH=amd64 go build -o ottopoint-purchase
 
-# scp -i ~/.ssh/LightsailDefaultKey-ap-southeast-1-new.pem ottopoint-purchase ubuntu@13.228.25.85:/home/ubuntu
+scp ottopoint-purchase rohmet@34.101.119.111:/home/build
 
-scp ottopoint-purchase ichsan@10.10.43.64:/home/ichsan
+# scp ottopoint-purchase ichsan@10.10.43.64:/home/ichsan
