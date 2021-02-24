068918a6a1c3454ce8fe4b33d832fe9b7d7111b9
Abdul Rohmat <abdul.rohmat@indoalliz.com>
2021-02-25 02:55:39 +0700

    fix - reversal point and update status transaction

--- services/v2/vouchers/check_status/sepulsa.go
+++ services/v2/vouchers/check_status/sepulsa.go
@@ -1,8 +1,16 @@
 package check_status
 
 import (
+	"encoding/json"
 	"fmt"
+	"ottopoint-purchase/constants"
+	"ottopoint-purchase/db"
+	kafka "ottopoint-purchase/hosts/publisher/host"
 	sepulsa "ottopoint-purchase/hosts/sepulsa/host"
+	"ottopoint-purchase/models"
+	"ottopoint-purchase/utils"
+
+	"ottopoint-purchase/services/v2.1/Trx"
 
 	"github.com/sirupsen/logrus"
 )
@@ -15,7 +23,7 @@ func CheckStatusSepulsaServices(trxid string) error {
 	logrus.Info(nameservice)
 
 	// check status ke sepulsa
-	_, errStatus := sepulsa.EwalletDetailTransaction(trxid)
+	resp, errStatus := sepulsa.EwalletDetailTransaction(trxid)
 	if errStatus != nil {
 
 		logrus.Error(nameservice)
@@ -24,6 +32,79 @@ func CheckStatusSepulsaServices(trxid string) error {
 
 		return errStatus
 	}
+
+	// Get Spending By TransactionID and OrderID
+	spending, err := db.GetSpendingSepulsa(resp.TransactionID, resp.OrderID)
+	if err != nil {
+		fmt.Println("[GetSpendingSepulsa] : ", err.Error())
+		logrus.Error("[ Failed Get SpendingSepulsa ] : ", err.Error())
+	}
+
+	responseCode := models.GetErrorMsg(resp.ResponseCode)
+
+	logrus.Info("[HandleSchedulerSepulsa] - [ResponseCode] : ", resp.ResponseCode)
+	logrus.Info("[HandleSchedulerSepulsa] - [ResponseDesc] : ", responseCode)
+
+	param := models.Params{
+		InstitutionID: spending.Institution,
+		NamaVoucher:   spending.Voucher,
+		AccountId:     spending.AccountId,
+		AccountNumber: spending.AccountNumber,
+		RRN:           spending.RRN,
+		TrxID:         utils.GenTransactionId(),
+		RewardID:      spending.MRewardID,
+		Point:         spending.Point,
+	}
+
+	header := models.RequestHeader{
+		DeviceID:      "ottopoint-purchase",
+		InstitutionID: spending.Institution,
+		Geolocation:   "-",
+		ChannelID:     "H2H",
+		AppsID:        "-",
+		Timestamp:     utils.GetTimeFormatYYMMDDHHMMSS(),
+		Authorization: "-",
+		Signature:     "-",
+	}
+
+	if (responseCode != "Success") && (responseCode != "Pending") {
+
+		resultReversal := Trx.V21_Adding_PointVoucher(param, spending.Point, 1, header)
+		fmt.Println(resultReversal)
+
+		fmt.Println("[ >>>>>>>>>>>>>>>>>>>>>>> Send Publisher <<<<<<<<<<<<<<<<<<<< ]")
+
+		pubreq := models.NotifPubreq{
+			Type:           constants.CODE_REVERSAL_POINT,
+			NotificationTo: spending.AccountNumber,
+			Institution:    spending.Institution,
+			ReferenceId:    spending.RRN,
+			TransactionId:  spending.TransactionId,
+			Data: models.DataValue{
+				RewardValue: "point",
+				Value:       fmt.Sprint(spending.Point),
+			},
+		}
+
+		bytePub, _ := json.Marshal(pubreq)
+
+		kafkaReq := kafka.PublishReq{
+			Topic: constants.TOPIC_PUSHNOTIF_GENERAL,
+			Value: bytePub,
+		}
+
+		kafkaRes, err := kafka.SendPublishKafka(kafkaReq)
+		if err != nil {
+			logrus.Error("Gagal Send Publisher : ", err)
+		}
+		logrus.Info("[ Response Publisher ] : ", kafkaRes)
+
+	}
+
+	responseSepulsa, _ := json.Marshal(resp)
+
+	// Update TSpending
+	go db.UpdateVoucherSepulsa(responseCode, resp.ResponseCode, string(responseSepulsa), resp.TransactionID, resp.OrderID)
 
 	return nil
 
