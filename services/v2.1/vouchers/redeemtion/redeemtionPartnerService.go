75b6966bd9a4baedd141d0e8c7e1c5ffd175dbb6
Abdul Rohmat <abdul.rohmat@indoalliz.com>
2021-05-10 21:15:17 +0700

    update response

--- services/v2.1/vouchers/redeemtion/redeemtionPartnerService.go
+++ services/v2.1/vouchers/redeemtion/redeemtionPartnerService.go
@@ -550,19 +550,16 @@ func RedeemtionOrder_V21_Services(req models.VoucherComultaiveReq, codeScheduler
 	}
 
 	var code, msg string
-	if req.Jumlah > 1 {
-		code, msg = msgRedeemtionCummulative(success, pending, failed)
-	} else {
-		if success != 0 {
-			code = "00"
-			msg = "Transaksi Berhasil"
-		} else if pending != 0 {
-			code = "09"
-			msg = "Transaksi Sedang Dalam Proses"
-		} else if failed != 0 {
-			code = "00"
-			msg = "Transaksi Gagal"
-		}
+
+	if success != 0 {
+		code = "00"
+		msg = "Transaksi Berhasil"
+	} else if failed != 0 {
+		code = "01"
+		msg = "Transaksi Gagal"
+	} else if pending != 0 {
+		code = "09"
+		msg = "Transaksi Sedang Dalam Proses"
 	}
 
 	res = models.Response{
